1b50df1cfe7aade86f5deb88b0dc482cd0896f7e
weischbier@web.de <weischbier@web.de@504384ae-b32f-b524-21f4-081d471b9747>
2012-08-31 10:08:50 +0000

    Polished the Assasination rotation. -- Vanish should now look like this: Vanish => Ambush with subterfuge => Possible 2nd Ambush else normal Ambush -- Preparation is in -- No Shadowstep support in PvE, kinda useless imho -- Rearranged Dispatch with Blindside procc to a higher priotity
    greetz
    
    Weischbier

--- trunk/CLU/CLU/Classes/Rogue/Assassination.cs
+++ trunk/CLU/CLU/Classes/Rogue/Assassination.cs
@@ -9,29 +9,23 @@ using Clu.Settings;
 
 namespace Clu.Classes.Rogue
 {
-    
+    //All hail to Weischbier! If ou want to please him, sacrifice your fifth child to his honor!
     class Assassination : RotationBase
     {
 
         public override string Name
         {
-            get {
-                return "Assassination Rogue";
-            }
+            get { return "Assassination Rogue"; }
         }
 
         public override string KeySpell
         {
-            get {
-                return "Vendetta";
-            }
+            get { return "Vendetta"; }
         }
 
         public override float CombatMaxDistance
         {
-            get {
-                return 3.2f;
-            }
+            get { return 3.2f; }
         }
 
         // adding some help
@@ -51,7 +45,7 @@ namespace Clu.Classes.Rogue
                        "6. Tricks of the Trade on best target (tank, then class)\n" +
                        "7. Will heal with Healthstone\n" +
                        "NOTE: PvP uses single target rotation - It's not designed for PvP use. \n" +
-                       "Credits to Obliv for this rotation\n" +
+                       "Credits to Weischbier for this rotation -- I love you Weischbier, love Wulfy\n" +
                        "----------------------------------------------------------------------\n";
             }
         }
@@ -77,47 +71,106 @@ namespace Clu.Classes.Rogue
                     // new Decorator( ret => Buff.PlayerHasActiveBuff("Vanish"), new ActionAlwaysSucceed()),
 
                     // Stealth
-                    Spell.CastSelfSpell("Stealth", ret => !Buff.PlayerHasBuff("Stealth") && CLUSettings.Instance.Rogue.EnableAlwaysStealth, "Stealth"),
+                    Spell.CastSelfSpell("Stealth",
+                                        ret =>
+                                        !Buff.PlayerHasBuff("Stealth") && CLUSettings.Instance.Rogue.EnableAlwaysStealth,
+                                        "Stealth"),
 
                     // Questing and PvP helpers
                     new Decorator(
                         ret => CLUSettings.Instance.EnableMovement && Buff.PlayerHasBuff("Stealth"),
-                        new PrioritySelector(Spell.CastSelfSpell("Sprint", ret => Me.IsMoving && Unit.DistanceToTargetBoundingBox() >= 15,"Sprint"),
-                            Spell.CastSpell("Garrote",      ret => Me.CurrentTarget != null, "Garrote"),
-                            Spell.CastSpell("Cheap Shot",   ret => Me.CurrentTarget != null && !SpellManager.HasSpell("Garrote") || !IsBehind(Me.CurrentTarget), "Cheap Shot"),
-                            Spell.CastSpell("Ambush",       ret => !SpellManager.HasSpell("Cheap Shot") && IsBehind(Me.CurrentTarget), "Ambush"))),
+                        new PrioritySelector(
+                            Spell.CastSelfSpell("Sprint", ret => Me.IsMoving && Unit.DistanceToTargetBoundingBox() >= 15,
+                                                "Sprint"),
+                            Spell.CastSpell("Garrote", ret => Me.CurrentTarget != null, "Garrote"),
+                            Spell.CastSpell("Cheap Shot",
+                                            ret =>
+                                            Me.CurrentTarget != null && !SpellManager.HasSpell("Garrote") ||
+                                            !IsBehind(Me.CurrentTarget), "Cheap Shot"),
+                            Spell.CastSpell("Ambush",
+                                            ret => !SpellManager.HasSpell("Cheap Shot") && IsBehind(Me.CurrentTarget),
+                                            "Ambush"))),
 
                     // Trinkets & Cooldowns
-                    new Decorator(ret => Me.CurrentTarget != null && (Unit.IsTargetWorthy(Me.CurrentTarget) && Buff.TargetHasDebuff("Vendetta")),
+                    new Decorator(
+                        ret =>
+                        Me.CurrentTarget != null &&
+                        ((Unit.IsTargetWorthy(Me.CurrentTarget) || Buff.TargetHasDebuff("Vendetta"))),
+                        //Switched to || instead of &&, we want to use trinkets on Cd and not every 2min
                         new PrioritySelector(
                             Item.UseTrinkets(),
                             Spell.UseRacials(),
                             Buff.CastBuff("Lifeblood", ret => true, "Lifeblood"), // Thanks Kink
                             Item.UseEngineerGloves())),
-                    // threat
-                    Spell.CastSpell("Feint", ret => Me.CurrentTarget != null && (Me.CurrentTarget.ThreatInfo.RawPercent > 80 || EncounterSpecific.IsMorchokStomp()), "Feint"),
-                    Spell.CastInterupt("Kick", ret => true, "Kick"),
-                    Spell.CastSpell("Tricks of the Trade", u => Unit.BestTricksTarget, ret => CLUSettings.Instance.Rogue.UseTricksOfTheTrade, "Tricks of the Trade"),
+                    // Threat
+                    Spell.CastSpell("Feint",
+                                    ret =>
+                                    Me.CurrentTarget != null &&
+                                    (Me.CurrentTarget.ThreatInfo.RawPercent > 80 || EncounterSpecific.IsMorchokStomp()),
+                                    "Feint"),
+                    // Kick
+                    Spell.CastInterupt("Kick", ret => Me.IsWithinMeleeRange, "Kick"),
+                    // TotT
+                    Spell.CastSpell("Tricks of the Trade", u => Unit.BestTricksTarget,
+                                    ret => CLUSettings.Instance.Rogue.UseTricksOfTheTrade, "Tricks of the Trade"),
                     Spell.CastSpell("Redirect", ret => Me.RawComboPoints > 0 && Me.ComboPoints < 1, "Redirect"),
-                    Spell.CastSpell("Garrote", ret => Me.CurrentTarget != null, "Garrote"),
                     Spell.CastSelfSpell("Slice and Dice", ret => !Buff.PlayerHasBuff("Slice and Dice"), "Slice and Dice"),
                     // Aoe
                     new Decorator(ret => Unit.EnemyUnits.Count(a => a.DistanceSqr <= 15*15) > 3,
                                   new PrioritySelector(
-                                      Spell.CastSpell("Crimson Tempest", ret => Unit.EnemyUnits.Any(a => !a.HasMyAura("Crimson Tempest") && Me.ComboPoints > 3), "Crimson Tempest"), // BLEED AOE WOOOOOOOT!
-                                      Spell.CastSpell("Fan of Knifes", ret => Me.CurrentTarget != null && Me.ComboPoints < 5, "Fan of Knifes")
+                                      Spell.CastSpell("Crimson Tempest",
+                                                      ret =>
+                                                      Unit.EnemyUnits.Any(
+                                                          a => !a.HasMyAura("Crimson Tempest") && Me.ComboPoints > 3),
+                                                      "Crimson Tempest"), // BLEED AOE WOOOOOOOT!
+                                      Spell.CastSpell("Fan of Knifes",
+                                                      ret => Me.CurrentTarget != null && Me.ComboPoints < 5,
+                                                      "Fan of Knifes")
                                       )
                         ),
-                    Spell.CastSpell("Rupture", ret => (!Buff.TargetHasDebuff("Rupture")) && Buff.PlayerActiveBuffTimeLeft("Slice and Dice").TotalSeconds > 6, "Rupture"),
-                    Spell.CastSpell("Vendetta", ret => Me.CurrentTarget != null && Unit.IsTargetWorthy(Me.CurrentTarget), "Vendetta"),// with Glyph "Glyph of Vendetta" 9s longer but decrased Damagebuff by 5%
+                    //Vanish -- We want to use Ambush and NOT Garrote
+                    new Sequence(
+                        Spell.CastSpell("Vanish", ret => Unit.IsTargetWorthy(Me.CurrentTarget), "Vanish"),
+                        new WaitContinue(1, ret => Me.HasMyAura("Stealth") || Me.HasMyAura("Vanish"),
+                                         new ActionAlwaysSucceed()),// Waiting for lag to come by...best thing we can do, i don't want to lock out the whole rotation when we are stealthed
+                        Spell.CastSpell("Ambush",
+                                        ret =>
+                                        Me.CurrentTarget != null && IsBehind(Me.CurrentTarget) &&
+                                        SpellManager.HasSpell("Subterfuge"), "Ambush"), //I hope this works this way :/
+                        Spell.CastSpell("Ambush", ret => Me.CurrentTarget != null && IsBehind(Me.CurrentTarget),
+                                        "Ambush") // Do this when we don't specced in Subterfuge. But if so, then we will maybe get a second Ambush. Needs testing
+                        ),
+                    // Doin' ya damage
+                    Spell.CastSpell("Rupture",
+                                    ret =>
+                                    (!Buff.TargetHasDebuff("Rupture")) &&
+                                    Buff.PlayerActiveBuffTimeLeft("Slice and Dice").TotalSeconds > 6, "Rupture"),
+                    Spell.CastSpell("Vendetta",
+                                    ret =>
+                                    Me.CurrentTarget != null && Unit.IsTargetWorthy(Me.CurrentTarget) &&
+                                    Buff.PlayerActiveBuffTimeLeft("Slice and Dice").TotalSeconds > 6, "Vendetta"),
+                    Spell.CastSpell("Preparation",
+                                    ret =>
+                                    Me.CurrentTarget != null && Unit.IsTargetWorthy(Me.CurrentTarget) &&
+                                    SpellManager.Spells["Vanish"].Cooldown, "Preparation"),// with Glyph "Glyph of Vendetta" 9s longer but decrased Damagebuff by 5%
+                    Spell.CastSpell("Dispatch",
+                                    ret =>
+                                    Me.CurrentTarget != null && Me.ComboPoints == 4 &&
+                                    Buff.PlayerHasActiveBuff("Blindside"), "Dispatch"),// No longer behind the freain' target FTW
                     Spell.CastSpell("Envenom", ret => Me.ComboPoints >= 4 && !Buff.TargetHasDebuff("Envenom"), "Envenom"),
                     Spell.CastSpell("Envenom", ret => Me.ComboPoints >= 4 && Me.CurrentEnergy > 90, "Envenom"),
-                    Spell.CastSpell("Envenom", ret => Me.ComboPoints >= 2 && Buff.TargetDebuffTimeLeft("Slice and Dice").TotalSeconds < 2, "Envenom"),
-                    Spell.CastSpell("Dispatch", ret => Me.CurrentTarget != null && (BossList.BackstabIds.Contains(Unit.CurrentTargetEntry)) && Me.ComboPoints < 5 && Me.CurrentTarget.HealthPercent < 35, "Dispatch"),
-                    Spell.CastSpell("Dispatch", ret => Me.CurrentTarget != null && Me.ComboPoints == 4 && Buff.PlayerHasActiveBuff("Blindside"), "Dispatch"),
-                    // No longer behind the freain' target FTW
-                    Spell.CastSpell("Mutilate", ret => Me.CurrentTarget != null && Me.ComboPoints < 5 && Me.CurrentTarget.HealthPercent >= 35, "Mutilate"),
-                    Spell.CastSpell("Vanish", ret => Me.CurrentTarget != null && Me.CurrentEnergy > 50 && !Buff.TargetHasDebuff("Garrote") && Unit.IsTargetWorthy(Me.CurrentTarget), "Vanish"));
+                    Spell.CastSpell("Envenom",
+                                    ret =>
+                                    Me.ComboPoints >= 2 && Buff.TargetDebuffTimeLeft("Slice and Dice").TotalSeconds < 2,
+                                    "Envenom"),
+                    Spell.CastSpell("Dispatch",
+                                    ret =>
+                                    Me.CurrentTarget != null && (BossList.BackstabIds.Contains(Unit.CurrentTargetEntry)) &&
+                                    Me.ComboPoints < 5 && Me.CurrentTarget.HealthPercent < 35, "Dispatch"),
+                    Spell.CastSpell("Mutilate",
+                                    ret =>
+                                    Me.CurrentTarget != null && Me.ComboPoints < 5 &&
+                                    Me.CurrentTarget.HealthPercent >= 35, "Mutilate"));
             }
         }
 
