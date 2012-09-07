8173b4d2b91d9cda79bf715cd919780e46d4a2fc
clutwopointzero@gmail.com <clutwopointzero@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-07 13:10:50 +0000

    Combat rogue updated..

--- trunk/CLU/CLU/Classes/Rogue/Combat.cs
+++ trunk/CLU/CLU/Classes/Rogue/Combat.cs
@@ -112,7 +112,7 @@ namespace CLU.Classes.Rogue
                            Spell.CastAreaSpell("Fan of Knives", 8, false, 6, 0.0, 0.0, ret => Me.CurrentEnergy > 85, "Fan of Knives"),
                            Spell.CastSpell("Tricks of the Trade", u => Unit.BestTricksTarget, ret => CLUSettings.Instance.Rogue.UseTricksOfTheTrade, "Tricks of the Trade"),
                            Spell.CastSpell("Expose Armor",                ret => Me.CurrentTarget != null && Me.ComboPoints == 5 && Unit.IsTargetWorthy(Me.CurrentTarget) && !Buff.UnitHasWeakenedArmor(Me.CurrentTarget), "Expose Armor"),
-                           Spell.CastSelfSpell("Slice and Dice",          ret => Buff.PlayerBuffTimeLeft("Slice and Dice") < 2, "Slice and Dice"),
+                           Spell.CastSelfSpell("Slice and Dice",          ret => Buff.PlayerBuffTimeLeft("Slice and Dice") < 2 && Me.CurrentEnergy >= 25 && Me.ComboPoints > 3, "Slice and Dice"),
                            Spell.CastSelfSpell("Killing Spree",           ret => Me.CurrentEnergy < 35 && Buff.PlayerBuffTimeLeft("Slice and Dice") > 4 && !Buff.PlayerHasBuff("Adrenaline Rush") && CLUSettings.Instance.UseCooldowns, "Killing Spree"),
                            Spell.CastSelfSpell("Adrenaline Rush",         ret => Me.CurrentTarget != null && Me.CurrentEnergy < 35 && Unit.IsTargetWorthy(Me.CurrentTarget), "Vanish"),
                            Spell.CastSpell("Eviscerate",                  ret => Me.ComboPoints == 5 && (Buff.PlayerHasBuff("Moderate Insight") || Buff.PlayerHasBuff("Deep Insight")), "Eviscerate & Moderate Insight or Deep Insight"),
