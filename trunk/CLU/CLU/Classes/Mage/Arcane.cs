c6f3117d245a3417d37a4da845b5bad9947326ae
weischbier@web.de <weischbier@web.de@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-08 15:30:08 +0000

    Arcane Mage by Weischbier Aoe bugs

--- trunk/CLU/CLU/Classes/Mage/Arcane.cs
+++ trunk/CLU/CLU/Classes/Mage/Arcane.cs
@@ -57,50 +57,57 @@ namespace CLU.Classes.Mage
 
         public override Composite SingleRotation
         {
-            get {
+            get
+            {
                 return new PrioritySelector(
-                           // Pause Rotation
-                           new Decorator(ret => CLUSettings.Instance.PauseRotation, new ActionAlwaysSucceed()),
+                    // Pause Rotation
+                    new Decorator(ret => CLUSettings.Instance.PauseRotation, new ActionAlwaysSucceed()),
 
-                           // For DS Encounters.
-                           EncounterSpecific.ExtraActionButton(),
+                    // For DS Encounters.
+                    EncounterSpecific.ExtraActionButton(),
 
-                           new Decorator(
-                               ret => Me.CurrentTarget != null && Unit.IsTargetWorthy(Me.CurrentTarget),
-                               new PrioritySelector(
-                                   Item.UseTrinkets(),
-                                   Spell.UseRacials(),
-                                   Buff.CastBuff("Lifeblood", ret => true, "Lifeblood"), // Thanks Kink
-                                   Item.UseBagItem("Volcanic Potion", ret => Buff.UnitHasHasteBuff(Me), "Volcanic Potion Heroism/Bloodlust"),
-                                   Item.UseEngineerGloves())),
-                           // Comment: Dont break Invinsibility!!
-                           new Decorator(
-                               x => Buff.PlayerHasBuff("Invisibility"), new Action(a => CLU.TroubleshootLog("Invisibility active"))),
-                           // Interupts & Steal Buffs
-                           Spell.CastSpell("Spellsteal",                  ret => Spell.TargetHasStealableBuff() && !Me.IsMoving, "[Steal] Spellsteal"),
-                           Spell.CastInterupt("Counterspell",             ret => true, "Counterspell"),
-                           // Cooldowns
-                           Spell.ChannelSelfSpell("Evocation",            ret => Me.ManaPercent < 35 && !Me.IsMoving, "Evocation"),
-                           Spell.CastSpell("Flame Orb",                   ret => Me.CurrentTarget != null && Unit.IsTargetWorthy(Me.CurrentTarget), "Flame Orb"),
-                           Item.UseBagItem("Mana Gem",                    ret => Me.CurrentTarget != null && Me.ManaPercent < 90 && Unit.IsTargetWorthy(Me.CurrentTarget), "Mana Gem"),
-                           Spell.CastSelfSpell("Mirror Image",            ret => Buff.PlayerHasBuff("Arcane Power") && Unit.IsTargetWorthy(Me.CurrentTarget), "Mirror Image"),
-                           Spell.CastSelfSpell("Presence of Mind",        ret => !Buff.PlayerHasBuff("Invisibility"), "Presence of Mind"),
-                           Spell.CastSelfSpell("Arcane Power",            ret => Me.CurrentTarget != null && Buff.PlayerHasBuff("Improved Mana Gem") || Unit.IsTargetWorthy(Me.CurrentTarget), "Arcane Power"),
-                           Item.RunMacroText("/cast Conjure Mana Gem",    ret => Buff.PlayerHasBuff("Presence of Mind") && !Item.HaveManaGem(), "Conjure Mana Gem"),
-                           // AoE
-                           new Decorator(
-                               ret => !Me.IsMoving && Unit.EnemyUnits.Count() > 2,
-                               new PrioritySelector(
-                                   Spell.CastSpell("Arcane Blast", ret => !Buff.PlayerHasBuff("Arcane Blast"), "Arcane Blast"),
-                                   Spell.CastAreaSpell("Arcane Explosion", 10, false, 4, 0.0, 0.0, ret => true, "Arcane Explosion"),
-                                   Spell.CastAreaSpell("Frost Nova", 10, false, 3, 0.0, 0.0, ret => true, "Frost Nova"))),
-                           // Default Rotaion
-                           Spell.CastSpell("Arcane Barrage",              ret => Spell.SpellCooldown("Evocation").TotalSeconds > 10 && Me.ManaPercent < 70 && !Buff.PlayerHasActiveBuff("Arcane Missiles!"), "Arcane Barrage"),
-                           Spell.ChannelSpell("Arcane Missiles",          ret => Spell.SpellCooldown("Evocation").TotalSeconds > 10 && Me.ManaPercent < 80 && Buff.PlayerHasActiveBuff("Arcane Missiles!"), "Arcane Missiles"),
-                           Spell.CastSpell("Arcane Blast",                ret => !Me.IsMoving, "Arcane Blast"),
-                           Spell.CastSpell("Arcane Barrage",              ret => Me.IsMoving, "Arcane Barrage (Moving)"),
-                           Spell.CastSpell("Fire Blast",                  ret => Me.IsMoving, "Fire Blast (Moving)"),
-                           Spell.CastSpell("Ice Lance",                   ret => Me.IsMoving, "Ice Lance (Moving)"));
+                    new Decorator(
+                        ret => Me.CurrentTarget != null && Unit.IsTargetWorthy(Me.CurrentTarget),
+                        new PrioritySelector(
+                            Item.UseTrinkets(),
+                            Spell.UseRacials(),
+                            Buff.CastBuff("Lifeblood", ret => true, "Lifeblood"), // Thanks Kink
+                            Item.UseBagItem("Volcanic Potion", ret => Buff.UnitHasHasteBuff(Me), "Volcanic Potion Heroism/Bloodlust"),
+                            Item.UseEngineerGloves())),
+                    // Comment: Dont break Invinsibility!!
+                    new Decorator(
+                        x => Buff.PlayerHasBuff("Invisibility"),
+                        new Action(a => CLU.TroubleshootLog("Invisibility active"))),
+                    // Interupts & Steal Buffs
+                    Spell.CastSpell("Spellsteal", ret => Spell.TargetHasStealableBuff() && !Me.IsMoving, "[Steal] Spellsteal"),
+                    Spell.CastInterupt("Counterspell", ret => true, "Counterspell"),
+                    // Cooldowns
+                    Spell.ChannelSelfSpell("Evocation", ret => Me.ManaPercent < 35 && !Me.IsMoving, "Evocation"),
+                    Item.UseBagItem("Mana Gem", ret => Me.CurrentTarget != null && Me.ManaPercent < 90 && Unit.IsTargetWorthy(Me.CurrentTarget), "Mana Gem"),
+                    Spell.CastSelfSpell("Mirror Image", ret => Buff.PlayerHasBuff("Arcane Power") && Unit.IsTargetWorthy(Me.CurrentTarget), "Mirror Image"),
+                    Spell.CastSelfSpell("Presence of Mind", ret => !Buff.PlayerHasBuff("Invisibility"), "Presence of Mind"),
+                    Spell.CastSelfSpell("Arcane Power", ret => Me.CurrentTarget != null && Buff.PlayerHasBuff("Improved Mana Gem") || Unit.IsTargetWorthy(Me.CurrentTarget), "Arcane Power"),
+                    Item.RunMacroText("/cast Conjure Mana Gem",ret => Buff.PlayerHasBuff("Presence of Mind") && !Item.HaveManaGem(),"Conjure Mana Gem"),
+                    // AoE
+                    new Decorator(ret => CLUSettings.Instance.UseAoEAbilities,
+                        new PrioritySelector(
+                            Spell.CastSpell("Fire Blast", ret => Me.CurrentTarget.HasMyAura("Living Bomb") && Unit.NearbyNonControlledUnits(Me.CurrentTarget.Location, 10f, false).Count(a => !a.IsFriendly) >= 3, "Fire Blast (Spreading the news...)"),
+                            Spell.CastSpell("Frost Bomb", ret => Unit.NearbyNonControlledUnits(Me.CurrentTarget.Location, 10f, false).Count(a => !a.IsFriendly) >= 3, "Frost Bomb"),
+                            Spell.CastSpellAtLocation("Flametrike", ret => Me.CurrentTarget, ret => Unit.NearbyNonControlledUnits(Me.CurrentTarget.Location, 10f, false).Count(a => !a.IsFriendly) >= 3, "Flamestrike"),
+                            Spell.CastSpell("Arcane Explosion",ret => Unit.NearbyNonControlledUnits(Me.Location, 10f, false).Count(a => !a.IsFriendly) >= 3,"Arcane Explsion")
+                        )
+                    ),
+                    //Damage
+                    //Tier1 Talent
+                    Buff.CastDebuff("Nether Tempest", ret => true, "Nether Tempest"),//Let's see how this turns out; Guides tell to refresh before LAST tick :/
+                    Buff.CastDebuff("Living Bomb", ret => true, "Living Bomb"),
+                    Spell.CastSpell("Frost Bomb", ret => true, "Frost Bomb"),
+                    Spell.CastSpell("Arcane Missiles", ret => Buff.GetAuraStack(Me, "Arcane Missiles!", true) == 2 || Buff.GetAuraStack(Me, "Arcane Charge", true) >= 5, "Arcane Missles"),
+                    Spell.CastSpell("Arcane Barrage", ret => Buff.GetAuraStack(Me, "Arcane Charge", true) > 4, "Arcane Barrage"),
+                    Spell.CastSpell("Arcane Blast", ret => Buff.GetAuraStack(Me, "Arcane Charge", true) < 6,"Arcane Blast")
+                    );
+
+
             }
         }
 
@@ -122,7 +129,7 @@ namespace CLU.Classes.Mage
                 return new Decorator(
                            ret => !Me.Mounted && !Me.IsDead && !Me.Combat && !Me.IsFlying && !Me.IsOnTransport && !Me.HasAura("Food") && !Me.HasAura("Drink"),
                            new PrioritySelector(
-                               Buff.CastBuff("Mage Armor",                     ret => true, "Mage Armor"),
+                               Buff.CastBuff("Frost Armor",                     ret => true, "Frost Armor"),
                                Buff.CastRaidBuff("Dalaran Brilliance",         ret => true, "Dalaran Brilliance"),
                                Buff.CastRaidBuff("Arcane Brilliance",          ret => true, "Arcane Brilliance"), 
                                Item.RunMacroText("/cast Conjure Mana Gem",     ret => !Me.IsMoving && !Item.HaveManaGem(), "Conjure Mana Gem")));
