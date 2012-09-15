44dd31b0bfd2e5c63853e7b558c0c9b616906f6c
clutwopointzero@gmail.com <clutwopointzero@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-15 04:15:20 +0000

    Addition of Common.HandleFlyingUnits for single target rotation // creation of Common.cs for warrios.

---
+++ trunk/CLU/CLU/Classes/Warrior/Common.cs
@@ -0,0 +1,37 @@
+﻿#region Revision info
+/*
+ * $Author: clutwopointzero@gmail.com $
+ * $Date$
+ * $ID$
+ * $Revision$
+ * $URL$
+ * $LastChangedBy$
+ * $ChangesMade$
+ */
+#endregion
+namespace CLU.Classes.Warrior
+{
+    using Styx;
+    using Styx.TreeSharp;
+
+    using global::CLU.Base;
+
+    /// <summary>
+    /// Common Warrior Functions.
+    /// </summary>
+    public static class Common
+    {
+        public static Composite HandleFlyingUnits
+        {
+            get
+            {
+                //Shoot flying targets
+                return new Decorator(
+                      ret => StyxWoW.Me.CurrentTarget.IsFlying,
+                      new PrioritySelector(
+                          Spell.CastSpell("Heroic Throw", ret => true, "Heroic Throw"),
+                          Spell.CastSpell("Throw", ret => true, "Throw")));
+            }
+        }
+    }
+}
