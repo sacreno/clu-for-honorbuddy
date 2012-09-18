b0fdb22b6404d1f9ab29d77ee2f2550009e7726b
Dagradt@Gmail.com <Dagradt@Gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-18 04:46:49 +0000

    Change made on "Mon, Sep 17, 2012 @ 11:46 by Dagradt to "Survival.cs"
    I added initial Survival rotation

--- trunk/CLU/CLU/Classes/Hunter/Survival.cs
+++ trunk/CLU/CLU/Classes/Hunter/Survival.cs
@@ -23,6 +23,7 @@ namespace CLU.Classes.Hunter
     using Styx.WoWInternals;
 
     using global::CLU.Managers;
+    using Styx.CommonBot;
 
     class Survival : RotationBase
     {
@@ -47,10 +48,12 @@ namespace CLU.Classes.Hunter
                 return "Explosive Shot";
             }
         }
+
         public override int KeySpellId
         {
             get { return 53301; }
         }
+
         public override float CombatMinDistance
         {
             get {
@@ -58,7 +61,6 @@ namespace CLU.Classes.Hunter
             }
         }
 
-        // adding some help
         public override string Help
         {
             get {
@@ -159,6 +161,86 @@ NOTE: PvP uses single target rotation - It's not designed for PvP use until Dagr
             }
         }
 
+        public Composite burstRotation
+        {
+            get
+            {
+                return (
+                    new PrioritySelector(
+                ));
+            }
+        }
+
+        public Composite baseRotation
+        {
+            get
+            {
+                return (
+                    new PrioritySelector(
+                        //new Action(a => { CLU.Log("I am the start of public Composite baseRotation"); return RunStatus.Failure; }),
+                        //PvP Utilities
+                        Spell.CastSpell("Concussive Shot", ret => Me.CurrentTarget != null && Me.CurrentTarget.DistanceSqr <= 25 * 25, "Concussive Shot"),
+                        Spell.CastSpell("Widow Venom", ret => !Buff.TargetHasDebuff("Widow Venom"), "Widow Venom"),
+                        Spell.CastSpell("Tranquilizing Shot", ret => Buff.TargetHasBuff("Enrage"), "Tranquilizing Shot"),
+                        Buff.CastBuff("Mend Pet", ret => Me.Pet.HealthPercent <= 90 && !Me.Pet.HasAura("Mend Pet"), "Mend Pet"),
+
+                        //Rotation
+                        //irmens_bite_potion,if=buff.bloodlust.react|target.time_to_die<=60
+                        //blood_fury
+                        Racials.UseRacials(),
+                        //aspect_of_the_hawk,moving=0
+                        Buff.CastBuff("Aspect of the Hawk", ret => !Me.IsMoving && !Buff.PlayerHasBuff("Aspect of the Hawk") && SpellManager.HasSpell("Aspect of the Hawk"), "Aspect of the Hawk"),
+                        Buff.CastBuff("Aspect of the Iron Hawk", ret => !Me.IsMoving && !Buff.PlayerHasBuff("Aspect of the Iron Hawk") && SpellManager.HasSpell("Aspect of the Iron Hawk"), "Aspect of the Iron Hawk"),
+                        //aspect_of_the_fox,moving=1
+                        Buff.CastBuff("Aspect of the Fox", ret => Me.IsMoving && !Buff.PlayerHasBuff("Aspect of the Fox"), "Aspect of the Fox"),
+                        //explosive_trap,if=target.adds>0
+                        Spell.HunterTrapBehavior("Explosive Trap", ret => Me.CurrentTarget, ret => Me.CurrentTarget != null && Me.CurrentTarget.DistanceSqr <= 40 * 40 && Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 10) > 0),
+                        //a_murder_of_crows,if=enabled&!ticking
+                        Spell.CastSpell("A Murder of Crows", ret => SpellManager.HasSpell("A Murder of Crows") && !Buff.TargetHasDebuff("A Murder of Crows"), "A Murder of Crows"),
+                        //blink_strike,if=enabled
+                        Spell.CastSpell("Blink Strike", ret => Me.CurrentTarget != null && Me.Pet.Location.DistanceSqr(Me.CurrentTarget.Location) <= 40 * 40 && Me.GotAlivePet && SpellManager.HasSpell("Blink Strike"), "Blink Strike"),
+                        //lynx_rush,if=enabled&!ticking
+                        Spell.CastSpell("Lynx Rush", ret => Me.CurrentTarget != null && Me.Pet.Location.DistanceSqr(Me.CurrentTarget.Location) <= 10 * 10 && Me.GotAlivePet && SpellManager.HasSpell("Lynx Rush") && !SpellManager.Spells["Lynx Rush"].Cooldown, "Lynx Rush"),
+                        //explosive_shot,if=buff.lock_and_load.react
+                        Spell.CastSpell("Explosive Shot", ret => Buff.PlayerHasActiveBuff("Lock and Load"), "Explosive Shot"),
+                        //glaive_toss,if=enabled
+                        Spell.CastSpell("Glaive Toss", ret => SpellManager.HasSpell("Glaive Toss"), "Glaive Toss"),
+                        //powershot,if=enabled
+                        Spell.CastSpell("Powershot", ret => SpellManager.HasSpell("Powershot"), "Powershot"),
+                        //barrage,if=enabled
+                        Spell.CastSpell("Barrage", ret => SpellManager.HasSpell("Barrage"), "Barage"),
+                        //multi_shot,if=target.adds>2
+                        //cobra_shot,if=target.adds>2
+                        //serpent_sting,if=!ticking&target.time_to_die>=10
+                        Spell.CastSpell("Serpent Sting", ret => !Buff.TargetHasDebuff("Serpent Sting"), "Serpent Sting"),
+                        //explosive_shot,if=cooldown_react
+                        Spell.CastSpell("Explosive Shot", ret => true, "Explosive Shot"),
+                        //kill_shot
+                        Spell.CastSpell("Kill Shot", ret => true, "Kill Shot"),
+                        //black_arrow,if=!ticking&target.time_to_die>=8
+                        Spell.CastSpell("Black Arrow", ret => !Buff.TargetHasDebuff("Black Arrow"), "Black Arrow"),
+                        //multi_shot,if=buff.thrill_of_the_hunt.react
+                        Spell.CastSpell("Multi Shot", ret => Buff.PlayerHasActiveBuff("Thrill of the Hunt"), "Multi Shot"),
+                        //dire_beast,if=enabled
+                        Spell.CastSpell("Dire Beast", ret => SpellManager.HasSpell("Dire Beast"), "Dire Beast"),
+                        //rapid_fire,if=!buff.rapid_fire.up
+                        Buff.CastBuff("Rapid Fire", ret => !Buff.PlayerHasActiveBuff("Rapid Fire"), "Rapid Fire"),
+                        //stampede
+                        Spell.CastSpell("Stampede", ret => true, "Stampede"),
+                        //readiness,wait_for_rapid_fire=1
+                        Spell.CastSelfSpell("Readiness", ret => Buff.PlayerHasActiveBuff("Rapid Fire"), "Readiness"),
+                        //fervor,if=enabled&focus<=50
+                        Buff.CastBuff("Fervor", ret => SpellManager.HasSpell("Feror") && Me.CurrentFocus <= 50, "Fervor"),
+                        //cobra_shot,if=dot.serpent_sting.remains<6
+                        Spell.CastSpell("Cobra Shot", ret => Buff.TargetDebuffTimeLeft("Serpent Sting").Seconds < 6, "Cobra Shot"),
+                        //arcane_shot,if=focus>=67
+                        Spell.CastSpell("Arcane Shot", ret => Me.CurrentFocus >= 67, "Arcane Shot"),
+                        //cobra_shot
+                        Spell.CastSpell("Cobra Shot", ret => true, "Cobra Shot")
+                ));
+            }
+        }
+
         public override Composite Medic
         {
             get {
@@ -178,11 +260,26 @@ NOTE: PvP uses single target rotation - It's not designed for PvP use until Dagr
 
         public override Composite PreCombat
         {
-            get {
-                return new Decorator(
-                           ret => !Me.Mounted && !Me.IsDead && !Me.Combat && !Me.IsFlying && !Me.IsOnTransport && !Me.HasAura("Food") && !Me.HasAura("Drink") && !Buff.PlayerHasBuff("Feign Death") && !Buff.PlayerHasBuff("Trap Launcher"),
-                           new PrioritySelector(
-                               Common.HunterCallPetBehavior(CLUSettings.Instance.Hunter.ReviveInCombat)));
+            get
+            {
+                return (
+                    new Decorator(ret => !Me.Mounted && !Me.IsDead && !Me.Combat && !Me.IsFlying && !Me.IsOnTransport && !Me.HasAura("Food") && !Me.HasAura("Drink") && !Buff.PlayerHasBuff("Feign Death"),
+                        new PrioritySelector(
+                            //flask,type=spring_blossoms
+                            //food,type=sea_mist_rice_noodles
+                            //hunters_mark,if=target.time_to_die>=21&!debuff.ranged_vulnerability.up
+                            Buff.CastDebuff("Hunter's Mark", ret => !TalentManager.HasGlyph("Marked for Death") || (CLU.LocationContext == GroupLogic.Battleground && Me.CurrentTarget != null && Me.CurrentTarget.DistanceSqr > 40 * 40), "Hunter's Mark"),
+                            //summon_pet
+                            Common.HunterCallPetBehavior(CLUSettings.Instance.Hunter.ReviveInCombat),
+                            //trueshot_aura
+                            //virmens_bite_potion
+                            new Action(delegate
+                            {
+                                Macro.isTrapMacroInUse();
+                                Macro.isMultiCastMacroInUse();
+                                return RunStatus.Failure;
+                            })
+                )));
             }
         }
 
@@ -195,8 +292,27 @@ NOTE: PvP uses single target rotation - It's not designed for PvP use until Dagr
 
         public override Composite PVPRotation
         {
-            get {
-                return this.SingleRotation;
+            get
+            {
+                return (
+                    new PrioritySelector(
+                    //new Action(a => { CLU.Log("I am the start of public override Composite PVPRotation"); return RunStatus.Failure; }),
+                        CrowdControl.freeMe(),
+                        new Decorator(ret => Macro.Manual || BotChecker.BotBaseInUse("BGBuddy"),
+                            new Decorator(ret => Me.CurrentTarget != null && Unit.IsTargetWorthy(Me.CurrentTarget),
+                                new PrioritySelector(
+                                    Item.UseTrinkets(),
+                                    Buff.CastBuff("Lifeblood", ret => true, "Lifeblood"),
+                                    Item.UseEngineerGloves(),
+                                    new Action(delegate
+                                    {
+                                        Macro.isTrapMacroInUse();
+                                        Macro.isMultiCastMacroInUse();
+                                        return RunStatus.Failure;
+                                    }),
+                                    new Decorator(ret => Macro.Burst && !Buff.PlayerHasBuff("Feign Death"), burstRotation),
+                                    new Decorator(ret => (!Macro.Burst || BotChecker.BotBaseInUse("BGBuddy")) && !Buff.PlayerHasBuff("Feign Death"), baseRotation)))
+                )));
             }
         }
 
