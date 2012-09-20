ee8cbeb6a17de523538d77ee2b70b7440baa2298
Dagradt@Gmail.com <Dagradt@Gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-20 14:41:26 +0000

    Change made on "Thur, Sep 20, 2012 @ 9:41 by Dagradt to "Shadow.cs"
    Fixed casting spells with a cast time spam

--- CLU/CLU/Classes/Priest/Shadow.cs
+++ CLU/CLU/Classes/Priest/Shadow.cs
@@ -188,13 +188,13 @@ NOTE: PvP uses single target rotation - It's not designed for PvP use until Dagr
                         //berserking
                         Racials.UseRacials(),
                         //mind_blast,if=num_targets<=6&cooldown_react
-                        Spell.CastSpell("Mind Blast", ret => Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 10) <= 6, "Mind Blast"),
+                        Spell.CastSpell("Mind Blast", ret => !Me.IsMoving && Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 10) <= 6, "Mind Blast"),
                         //shadow_word_pain,cycle_targets=1,max_cycle_targets=8,if=(!ticking|remains<tick_time)&miss_react
                         Spell.CastSpell("Shadow Word: Pain", ret => !Buff.TargetHasDebuff("Shadow Word: Pain") || Buff.TargetDebuffTimeLeft("Shadow Word: Pain").TotalSeconds < 2.52, "Shadow Word: Pain"),//~> GUI option for tick and cast time
                         //shadow_word_death,if=num_targets<=5
                         Spell.CastSpell("Shadow Word: Death", ret => Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 10) <= 5, "Shadow Word: Death"),
                         //vampiric_touch,cycle_targets=1,max_cycle_targets=8,if=(!ticking|remains<cast_time+tick_time)&miss_react
-                        Spell.CastSpell("Vampiric Touch", ret => !Buff.TargetHasDebuff("Vampiric Touch") || Buff.TargetDebuffTimeLeft("Vampiric Touch").TotalSeconds < 1.26 + 2.52, "Vampiric Touch"),//~> GUI option for tick and cast time
+                        Spell.CastSpell("Vampiric Touch", ret => !Me.IsMoving && !Buff.TargetHasDebuff("Vampiric Touch") || Buff.TargetDebuffTimeLeft("Vampiric Touch").TotalSeconds < 1.26 + 2.52, "Vampiric Touch"),//~> GUI option for tick and cast time
                         //devouring_plague,if=shadow_orb=3
                         Spell.CastSpell("Devouring Plague", ret => Buff.PlayerCountBuff("Shadow Orb") == 3, "Devouring Plague"),
                         //H	11.10	halo_damage
@@ -203,9 +203,9 @@ NOTE: PvP uses single target rotation - It's not designed for PvP use until Dagr
                         //shadowfiend,if=cooldown_react
                         Spell.CastSpell("Shadowfiend", ret => true, "Shadowfiend"),
                         //mind_sear,chain=1,interrupt=1,if=num_targets>=3
-                        Spell.CastSpell("Mind Sear", ret => Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 10) >= 3, "Mind Sear"),
+                        Spell.CastSpell("Mind Sear", ret => !Me.IsMoving && Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 10) >= 3, "Mind Sear"),
                         //mind_flay,chain=1,interrupt=1
-                        Spell.ChannelSpell("Mind Flay", ret => true, "Mind Flay"),
+                        Spell.ChannelSpell("Mind Flay", ret => !Me.IsMoving, "Mind Flay"),
                         //shadow_word_death,moving=1
                         Spell.CastSpell("Shadow Word: Death", ret => Me.IsMoving, "Shadow Word: Death"),
                         //mind_blast,moving=1,if=buff.divine_insight_shadow.react&cooldown_react
