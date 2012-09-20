db5bbcda7caf06b7d59a6e3b84cf76f0a07f058c
clutwopointzero@gmail.com <clutwopointzero@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-20 07:42:48 +0000

    Updates for incomming Druid additions.

--- CLU/CLU/CombatLog/WoWStats.cs
+++ CLU/CLU/CombatLog/WoWStats.cs
@@ -100,6 +100,7 @@ namespace CLU.CombatLog
             // get the english spell name, not the localized one!
             var spellID = Convert.ToInt32(args[4]);
             var spell = WoWSpell.FromId(spellID).Name;
+            var sourceGuid = ulong.Parse(args[3].ToString().Replace("0x", string.Empty), NumberStyles.HexNumber);
 
             // We need to 'sleep' for these spells. Otherwise, we'll end up double-casting them. Which will cause issues
             switch (spell) {
@@ -115,6 +116,13 @@ namespace CLU.CombatLog
             case "Holy Shock":
                 CLU.DiagnosticLog("Sleeping for heal success. ({0})", spell);
                 StyxWoW.SleepForLagDuration();
+                break;
+            case "Nature's Swiftness":
+                CLU.DiagnosticLog("PrevNaturesSwiftness. ({0})", spell);
+                if (sourceGuid == StyxWoW.Me.Guid)
+                {
+                    Classes.Druid.Common.PrevNaturesSwiftness = spellID == 132158;
+                }
                 break;
             }
 
