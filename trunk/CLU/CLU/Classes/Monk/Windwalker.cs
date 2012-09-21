c98d3b956dc3c38a4b0489cfa97e17b282387616
clutwopointzero@gmail.com <clutwopointzero@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-21 01:58:45 +0000

    Update to windwalker //thanks kbrebel04 Possible addition of mangle fix.

--- trunk/CLU/CLU/Classes/Monk/Windwalker.cs
+++ trunk/CLU/CLU/Classes/Monk/Windwalker.cs
@@ -121,35 +121,28 @@ namespace CLU.Classes.Monk
                                    Item.UseEngineerGloves())),
 
                            //Single Target
-                           Spell.CastSpell("Chi Sphere", ret => TalentManager.HasTalent(7) && Chi <= 4, "Chi Sphere"),
-                           Spell.CastSpell("Chi Brew", ret => TalentManager.HasTalent(9) && Chi < 0, "Chi Brew"),
-                           Spell.CastSpell("Rising Sun Kick", ret => !Buff.TargetHasDebuff("Rising Sun Kick") || Buff.TargetCountDebuff("Rising Sun Kick") < 3, "Rising Sun Kick"),
+                           Spell.CastSelfSpell("Tigereye Brew", ret => !Buff.PlayerHasBuff("Tigereye Brew Use") && Buff.PlayerCountBuff("Tigereye Brew") == 10 && CLUSettings.Instance.UseCooldowns, "Tigerye Brew"),
+                           Spell.CastSpell("Chi Brew", ret => TalentManager.HasTalent(9) && Chi <= 0 && Buff.PlayerHasBuff("Tigereye Brew Use") && Me.CurrentEnergy <= 50, "Chi Brew"),
+                           Spell.CastSpell("Energizing Brew", ret => Me.CurrentEnergy <= 30, "Energizing Brew"),
+                           Spell.CastSpell("Touch of Death", ret => true, "Touch of Death"),
+                           Spell.CastSpell("Rising Sun Kick", ret => true, "Rising Sun Kick"),
                            Spell.CastSpell("Tiger Plam", ret => Buff.PlayerCountBuff("Tiger Power") < 3 || Buff.PlayerBuffTimeLeft("Tiger Power") < 3, "Tiger Power"),
-                           Spell.CastSpell("Tigereye Brew", ret => !Buff.PlayerHasBuff("Tigereye Brew Use") && Buff.PlayerCountBuff("Tigereye Brew") > 10 && CLUSettings.Instance.UseCooldowns, "Tigerye Brew"),
-                           //Spell.CastSpell("Energizing Brew",   ret => Buff. //Get back to this later.
-                           Spell.CastSelfSpell("Invoke Xuen, the White Tiger", ret => TalentManager.HasTalent(17), "Invoke Xuen"),
-                           Spell.CastSpell("Rushing Jade Wind", ret => TalentManager.HasTalent(16), "Rushing Jade Wind"),
-                           Spell.CastSpell("Fists of Fury", ret => !Buff.PlayerHasActiveBuff("Energizing Brew") && Me.CurrentEnergy > 80 && Buff.PlayerBuffTimeLeft("Tiger Power") > 4 && Buff.PlayerCountBuff("Tiger Power") > 3, "Fists of Fury"),
-                           Spell.CastSpell("Blackout Kick", ret => Buff.PlayerHasActiveBuff("Combo Breaker"), "Blackout Kick"),
-                           Spell.CastSpell("Blackout Kick", ret => Chi > 3 && Me.CurrentEnergy > 50, "Blackout Kick"),
-                           Spell.CastSpell("Tiger Palm", ret => (Buff.PlayerHasActiveBuff("Combo Breaker") && Me.CurrentEnergy > 70) || (Buff.PlayerBuffTimeLeft("Combo Breaker") < 2 && Buff.PlayerHasActiveBuff("Combo Breaker")), "Tiger Palm"),
-                           Spell.CastSpell("Jab", ret => Buff.PlayerHasActiveBuff("Ascension") && Chi < 3, "Jab"),
-                           Spell.CastSpell("Jab", ret => Buff.PlayerHasActiveBuff("Chi Brew") && Chi < 2, "Jab"),
-                           Spell.CastSpell("Blackout Kick", ret => (Me.CurrentEnergy > 30 && RisingSunKickCoolDown) || (Chi > 4 && Buff.PlayerHasActiveBuff("Ascension")) || (Chi > 5 && Buff.PlayerHasActiveBuff("Ascension")), "Blackout Kick"),
+                           Spell.CastSelfSpell("Invoke Xuen, the White Tiger", ret => TalentManager.HasTalent(17) && Buff.PlayerCountBuff("Tiger Power") == 3 && Buff.TargetHasDebuff("Rising Sun Kick") && Me.CurrentEnergy <= 80, "Invoke Xuen"),
+                           Spell.CastSpell("Rushing Jade Wind", ret => TalentManager.HasTalent(16) && Buff.PlayerCountBuff("Tiger Power") == 3 && Buff.TargetHasDebuff("Rising Sun Kick") && Me.CurrentEnergy <= 80, "Rushing Jade Wind"),
+                           Spell.CastSpell("Fists of Fury", ret => !Me.IsMoving && !Buff.PlayerHasActiveBuff("Energizing Brew") && Me.CurrentEnergy <= 50 && Buff.PlayerBuffTimeLeft("Tiger Power") > 4 && Buff.PlayerCountBuff("Tiger Power") == 3 && Spell.SpellCooldown("Rising Sun Kick").TotalSeconds >= 2, "Fists of Fury"),
+                           Spell.CastSpell("Blackout Kick", ret => Buff.PlayerHasActiveBuff("Combo Breaker: Blackout Kick"), "Blackout Kick"),
+                           Spell.CastSpell("Tiger Palm", ret => (Buff.PlayerHasActiveBuff("Combo Breaker: Tiger Palm") && Me.CurrentEnergy < 90) || (Buff.PlayerBuffTimeLeft("Combo Breaker: Tiger Palm") < 2 && Buff.PlayerHasActiveBuff("Combo Breaker: Tiger Palm")), "Tiger Palm"),
+                           Spell.CastSpell("Blackout Kick", ret => Chi >= 2 && Spell.SpellOnCooldown("Fists of Fury") || Chi >= 3 && Me.CurrentEnergy <= 50 && Me.IsMoving && !Spell.SpellOnCooldown("Fists of Fury") || Chi >= 3 && Me.CurrentEnergy > 50, "Blackout Kick"),
+                           Spell.CastSpell("Jab", ret => TalentManager.HasTalent(8) && Chi <= 3, "Jab"),
+                           Spell.CastSpell("Jab", ret => Chi <= 2, "Jab"),
+                            //Spell.CastSpell("Blackout Kick", ret => (Me.CurrentEnergy > 30 && RisingSunKickCoolDown) || (Chi > 4 && Buff.PlayerHasActiveBuff("Ascension")) || (Chi > 5 && Buff.PlayerHasActiveBuff("Ascension")), "Blackout Kick"),
 
                            // Interupt
                            Spell.CastInterupt("Spear Hand Strike", ret => true, "Spear Hand Strike"),
-                           Spell.CastSelfSpell("Tigereye Brew", ret => Buff.PlayerCountBuff("Tigereye Brew") == 10 && CLUSettings.Instance.UseCooldowns, "Tigereye Brew"),
-                           Spell.CastSpell("Rising Sun Kick", ret => true, "Rising Sun Kick"),
+
                             // AoE
                            Spell.CastAreaSpell("Fists of Fury", 8, false, 3, 0.0, 0.0, ret => true, "Fists of Fury"),
-                           Spell.CastAreaSpell("Spinning Crane Kick", 8, false, 3, 0.0, 0.0, ret => true, "Spinning Crane Kick"), // no really required but will leave in for low levels
-                           Spell.CastSelfSpell("Invoke Xuen, the White Tiger", ret => TalentManager.HasTalent(17) && CLUSettings.Instance.UseCooldowns, "Invoke Xuen, the White Tiger"), // if=talent.invoke_xuen.enabled (TalentManager.HasTalent(1, 1) is bogus but I put it in here to remind me to check for this to be talented)
-                           Spell.CastSpell("Blackout Kick", ret => Buff.PlayerHasActiveBuff("Combo Breaker: Blackout Kick"), "Blackout Kick"),
-                           Spell.CastSpell("Tiger Palm", ret => Buff.PlayerHasActiveBuff("Combo Breaker: Tiger Palm"), "Tiger Palm"),
-                           Spell.CastSpell("Blackout Kick", ret => Chi >= 1, "Blackout Kick"),
-                            // Spells.CastSpell("TigerPalm",             ret => Buff.PlayerCountBuff("Tiger Power") == 3, "TigerPalm"), //posible dps loss
-                            // Spells.CastSpell("TigerPalm",             ret => Buff.PlayerCountBuff("Tiger Power") <= 3, "TigerPalm"), //posible dps loss
+                           Spell.CastAreaSpell("Spinning Crane Kick", 8, false, 3, 0.0, 0.0, ret => true, "Spinning Crane Kick"),
                            Spell.CastSpell(JabSpellList.Find(SpellManager.CanCast), ret => true, "JabSpell"));
             }
         }
