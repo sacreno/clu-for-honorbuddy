c2bab5162bf72e584218147f4bb27dbb6b1ec7c2
Dagradt@Gmail.com <Dagradt@Gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-22 17:40:01 +0000

    Hunter tweaks and new trapbehavior method for PvP, will cast Scatter to root target for clean trap application

--- trunk/CLU/CLU/Base/Macro.cs
+++ trunk/CLU/CLU/Base/Macro.cs
@@ -15,6 +15,10 @@ using Styx.WoWInternals;
 using Styx;
 using Styx.CommonBot;
 using Styx.WoWInternals.WoWObjects;
+using Styx.TreeSharp;
+using CLU.Settings;
+using Action = Styx.TreeSharp.Action;
+using CommonBehaviors.Actions;
 
 namespace CLU.Base
 {
@@ -81,8 +85,6 @@ namespace CLU.Base
             {
                 resetMacro("MultiCastFT");
                 resetMacro("MultiCastMT");
-                resetMacro("TrapMT");
-                resetMacro("TrapFT");
             }
         }
 
@@ -166,112 +168,6 @@ namespace CLU.Base
             }
         }
 
-        static int TrapMT = 0;
-        static int TrapFT = 0;
-        static string whatTrap;
-        static WoWSpell _Trap;
-        static bool shouldUseScatter = true;//~> Replace with GUI option
-        static bool _usedScatter = false;
-        
-        /// <summary>
-        /// Main Trap call: checks if Trap MT or FT is active, also checks for LoS and if we can cast the specified spell
-        /// If zero(0) is present: return to primary or previous rotation
-        /// If one(1) is present: run MT or FT then return to primary or previous rotation
-        /// </summary>
-        public static void isTrapMacroInUse()
-        {
-            using (StyxWoW.Memory.AcquireFrame())
-            {
-                TrapMT = Lua.GetReturnVal<int>("return TrapMT", 0);
-                TrapFT = Lua.GetReturnVal<int>("return TrapFT", 0);
-                whatTrap = Lua.GetReturnVal<String>("return TrapType", 0);
-            }
-            if (TrapMT > 0 || TrapFT > 0)
-            {
-                if (whatTrap == null)
-                {
-                    CLU.Log("Invalid Trap");
-                    resetAllMacros();
-                }
-                else
-                {
-                    SpellManager.Spells.TryGetValue(whatTrap, out _Trap);
-                    if (!_Trap.IsValid || _Trap.CooldownTimeLeft > SpellManager.GlobalCooldownLeft)
-                    {
-                        CLU.Log("Can't cast trap");
-                        resetAllMacros();
-                    }
-                    else if (Me.CurrentFocus <= 20 && !Buff.PlayerHasActiveBuff("Trap Launcher"))
-                    {
-                        CLU.Log("Can't cast trap launcher");
-                        resetAllMacros();
-                    }
-                }
-            }
-            if (TrapMT > 0)
-            {
-                if (SpellManager.GlobalCooldown || Me.IsCasting)
-                    return;
-                if (Me.CurrentTarget.InLineOfSight)
-                {
-                    if (_usedScatter == false && shouldUseScatter == true && SpellManager.CanCast("Scatter Shot"))//cancast on current target
-                    {
-                        _usedScatter = true;
-                        Spell.CastSpell("Scatter Shot", ret => Me.CurrentTarget, ret => true, "Scatter Shot");
-                        return;
-                    }
-                    if (SpellManager.CanCast("Trap Launcher"))
-                    {
-                        Buff.CastBuff("Trap Launcher", ret => true, "Trap Launcher");
-                        return;
-                    }
-                    if (SpellManager.CanCast(whatTrap) && Buff.PlayerHasBuff("Trap Launcher"))
-                    {
-                        Lua.DoString("RunMacroText(\"/cast " + whatTrap + "\")");
-                        SpellManager.ClickRemoteLocation(Me.CurrentTarget.Location);
-                        resetMacro("TrapMT");
-                        _usedScatter = false;
-                    }
-                }
-                else
-                {
-                    resetMacro("TrapMT");
-                    _usedScatter = false;
-                }
-            }
-            else if (TrapFT > 0)
-            {
-                if (SpellManager.GlobalCooldown || Me.IsCasting)
-                    return;
-                if (Me.FocusedUnit.InLineOfSight)
-                {
-                    if (_usedScatter == false && shouldUseScatter == true && SpellManager.CanCast("Scatter Shot"))//cancast on focused target
-                    {
-                        _usedScatter = true;
-                        Spell.CastSpell("Scatter Shot", ret => Me.FocusedUnit, ret => true, "Scatter Shot");
-                        return;
-                    }
-                    if (SpellManager.CanCast("Trap Launcher"))
-                    {
-                        Buff.CastBuff("Trap Launcher", ret => true, "Trap Launcher");
-                        return;
-                    }
-                    if (SpellManager.CanCast(whatTrap) && Buff.PlayerHasBuff("Trap Launcher"))
-                    {
-                        Lua.DoString("RunMacroText(\"/cast [@focus] " + whatTrap + "\")");
-                        SpellManager.ClickRemoteLocation(Me.FocusedUnit.Location);
-                        resetMacro("TrapFT");
-                        _usedScatter = false;
-                    }
-                }
-                else
-                {
-                    resetMacro("TrapFT");
-                    _usedScatter = false;
-                }
-            }
-        }
-
         /// <summary>
         /// Checks to see if MultiCast MT or FT are active
         /// </summary>
@@ -283,6 +179,34 @@ namespace CLU.Base
                 return false;
             }
             return true;
+        }
+
+        /// <summary>
+        /// Sets a trap at the specified targets location
+        /// </summary>
+        /// <param name="trapName">The name of the trap to use</param>
+        /// <param name="onUnit">The unit to place the trap on</param>
+        /// <param name="cond">Check conditions supplied are true</param>
+        /// <returns></returns>
+        public static Composite pvpTrapBehavior(string trapName, CLU.UnitSelection onUnit, CanRunDecoratorDelegate cond)
+        {
+            return (
+                new PrioritySelector(
+                    new Decorator(delegate(object a)
+                        {
+                            if (!cond(a))
+                                return false;
+                            return onUnit != null && onUnit(a) != null && onUnit(a).DistanceSqr <= 20 * 20 && SpellManager.HasSpell(trapName) && !SpellManager.Spells[trapName].Cooldown;
+                        },
+                new PrioritySelector(
+                    Buff.CastBuff("Trap Launcher", ret => !Buff.PlayerHasBuff("Trap Launcher"), "Trap Launcher"),
+                    Spell.CastSpell("Scatter Shot", ret => true, "Scatter Shot"),
+                    new Decorator(ret => Buff.PlayerHasBuff("Trap Launcher"),
+                        new Sequence(
+                            new Action(ret => Lua.DoString(string.Format("CastSpellByName(\"{0}\")", trapName))),
+                            new WaitContinue(TimeSpan.FromMilliseconds(200), ret => false, new ActionAlwaysSucceed()),
+                            new Action(ret => SpellManager.ClickRemoteLocation(onUnit(ret).Location)))))
+            )));
         }
     }
 }
