8b9bccfce114b07608359f55365f0f992ac19acc
clutwopointzero@gmail.com <clutwopointzero@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-22 22:07:51 +0000

    Fix for aspect spamming.

--- trunk/CLU/CLU/Classes/Hunter/Common.cs
+++ trunk/CLU/CLU/Classes/Hunter/Common.cs
@@ -54,12 +54,14 @@ namespace CLU.Classes.Hunter
                             new WaitContinue(2, ret => false, new ActionAlwaysSucceed()), // Hmm..check this...-- wulf
                             Buff.CastBuff("Aspect of the Fox", ret => Me.IsMoving, "[Aspect] of the Fox - Moving"),
                             new PrioritySelector(
-                                Buff.CastBuff("Aspect of the Hawk", ret => !Me.IsMoving, "[Aspect] of the Hawk")
+                                Buff.CastBuff("Aspect of the Hawk", ret => !Me.IsMoving && !TalentManager.HasTalent(8), "[Aspect] of the Hawk"),
+                                Buff.CastBuff("Aspect of the Hawk", "Aspect of the Iron Hawk", ret => !Me.IsMoving && TalentManager.HasTalent(8), "Aspect of the Iron Hawk")
                                 )
                                 )),
                     new Decorator( ret => !Me.IsMoving && CLUSettings.Instance.Hunter.HandleAspectSwitching,
                         new PrioritySelector(
-                                Buff.CastBuff("Aspect of the Hawk", ret => !Me.IsMoving, "[Aspect] of the Hawk")
+                                Buff.CastBuff("Aspect of the Hawk", ret => !Me.IsMoving && !TalentManager.HasTalent(8), "[Aspect] of the Hawk"),
+                                Buff.CastBuff("Aspect of the Hawk", "Aspect of the Iron Hawk", ret => !Me.IsMoving && TalentManager.HasTalent(8), "Aspect of the Iron Hawk")
                                 )));
         }
 
