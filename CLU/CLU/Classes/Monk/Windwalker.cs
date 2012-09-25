61b043e9578c136f102903fb112a6d15995c4ca7
clutwopointzero@gmail.com <clutwopointzero@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-25 21:25:21 +0000

    Update to monk //thanks Kbreberl04

--- CLU/CLU/Classes/Monk/Windwalker.cs
+++ CLU/CLU/Classes/Monk/Windwalker.cs
@@ -66,18 +66,7 @@ namespace CLU.Classes.Monk
             get
             {
                 return "----------------------------------------------------------------------\n" +
-                       "This Rotation will:\n" +
-                       "1. \n" +
-                       "==> \n" +
-                       "2. AutomaticCooldowns has: \n" +
-                       "==> UseTrinkets \n" +
-                       "==> UseRacials \n" +
-                       "==> UseEngineerGloves \n" +
-                       "==> \n" +
-                       "3. \n" +
-                       "4. Best Suited for end game raiding\n" +
-                       "NOTE: PvP uses single target rotation - It's not designed for PvP use. \n" +
-                       "Credits: alxaw for extending the basic rotation" +
+                       "Credits: alxaw, kbrebel04" +
                        "----------------------------------------------------------------------\n";
             }
         }
@@ -122,19 +111,20 @@ namespace CLU.Classes.Monk
 
                            //Single Target
                            Spell.CastSelfSpell("Tigereye Brew", ret => !Buff.PlayerHasBuff("Tigereye Brew Use") && Buff.PlayerCountBuff("Tigereye Brew") == 10 && CLUSettings.Instance.UseCooldowns, "Tigerye Brew"),
-                           Spell.CastSpell("Chi Brew", ret => TalentManager.HasTalent(9) && Chi <= 0 && Buff.PlayerHasBuff("Tigereye Brew Use") && Me.CurrentEnergy <= 50, "Chi Brew"),
+                            //Spell.CastSpell("Chi Brew", ret => TalentManager.HasTalent(9) && Buff.PlayerHasBuff("Tigereye Brew Use" && Chi == 0 && Me.CurrentEnergy <= 50, "Chi Brew"),     is not identifying "tigereye brew use" buff
                            Spell.CastSpell("Energizing Brew", ret => Me.CurrentEnergy <= 30, "Energizing Brew"),
                            Spell.CastSpell("Touch of Death", ret => true, "Touch of Death"),
-                           Spell.CastSpell("Rising Sun Kick", ret => true, "Rising Sun Kick"),
-                           Spell.CastSpell("Tiger Palm", ret => Buff.PlayerCountBuff("Tiger Power") < 3 || Buff.PlayerBuffTimeLeft("Tiger Power") < 3, "Tiger Palm"),
+                           Spell.CastSpell("Rising Sun Kick", ret => !Buff.TargetHasDebuff("Rising Sun Kick") || Buff.PlayerBuffTimeLeft("Tiger Power") < 5 && Buff.PlayerBuffTimeLeft("Tiger Power") > 3 && Me.CurrentEnergy >= 40 || Buff.PlayerBuffTimeLeft("Tiger Power") >= 5, "Rising Sun Kick"),
+                           Spell.CastSpell("Tiger Palm", ret => Buff.PlayerCountBuff("Tiger Power") < 3 || Buff.PlayerBuffTimeLeft("Tiger Power") <= 3, "Tiger Palm"),
                            Spell.CastSelfSpell("Invoke Xuen, the White Tiger", ret => TalentManager.HasTalent(17) && Buff.PlayerCountBuff("Tiger Power") == 3 && Buff.TargetHasDebuff("Rising Sun Kick") && Me.CurrentEnergy <= 80, "Invoke Xuen"),
                            Spell.CastSpell("Rushing Jade Wind", ret => TalentManager.HasTalent(16) && Buff.PlayerCountBuff("Tiger Power") == 3 && Buff.TargetHasDebuff("Rising Sun Kick") && Me.CurrentEnergy <= 80, "Rushing Jade Wind"),
-                           Spell.CastSpell("Fists of Fury", ret => !Me.IsMoving && !Buff.PlayerHasActiveBuff("Energizing Brew") && Me.CurrentEnergy <= 50 && Buff.PlayerBuffTimeLeft("Tiger Power") > 4 && Buff.PlayerCountBuff("Tiger Power") == 3 && Spell.SpellCooldown("Rising Sun Kick").TotalSeconds >= 2, "Fists of Fury"),
+                           Spell.CastSpell("Fists of Fury", ret => !Me.IsMoving && !Buff.PlayerHasActiveBuff("Energizing Brew") && Me.CurrentEnergy <= 50 && Buff.PlayerBuffTimeLeft("Tiger Power") > 5 && Buff.PlayerCountBuff("Tiger Power") == 3 && Spell.SpellCooldown("Rising Sun Kick").TotalSeconds >= 2, "Fists of Fury"),
                            Spell.CastSpell("Blackout Kick", ret => Buff.PlayerHasActiveBuff("Combo Breaker: Blackout Kick"), "Blackout Kick"),
                            Spell.CastSpell("Tiger Palm", ret => (Buff.PlayerHasActiveBuff("Combo Breaker: Tiger Palm") && Me.CurrentEnergy < 90) || (Buff.PlayerBuffTimeLeft("Combo Breaker: Tiger Palm") < 2 && Buff.PlayerHasActiveBuff("Combo Breaker: Tiger Palm")), "Tiger Palm"),
-                           Spell.CastSpell("Blackout Kick", ret => Chi >= 2 && Spell.SpellOnCooldown("Fists of Fury") || Chi >= 3 && Me.CurrentEnergy <= 50 && Me.IsMoving && !Spell.SpellOnCooldown("Fists of Fury") || Chi >= 3 && Me.CurrentEnergy > 50, "Blackout Kick"),
-                           Spell.CastSpell("Jab", ret => TalentManager.HasTalent(8) && Chi <= 3, "Jab"),
-                           Spell.CastSpell("Jab", ret => Chi <= 2, "Jab"),
+                           Spell.CastSpell("Blackout Kick", ret => Chi >= 2 && Spell.SpellOnCooldown("Fists of Fury") && SpellManager.Spells["Rising Sun Kick"].CooldownTimeLeft.Seconds >= 2 || Chi >= 3 && Me.CurrentEnergy <= 50 && Me.IsMoving && !Spell.SpellOnCooldown("Fists of Fury") && SpellManager.Spells["Rising Sun Kick"].CooldownTimeLeft.Seconds >= 2 || Chi >= 3 && Me.CurrentEnergy > 50 && SpellManager.Spells["Rising Sun Kick"].CooldownTimeLeft.Seconds >= 2, "Blackout Kick"),
+                            //Spell.CastSpell("Jab", ret => TalentManager.HasTalent(8) && Chi <= 3, "Jab"), is not identifying the talent, is using jab at 3 chi without the talent
+                           Spell.CastSpell("Expel Harm", ret => Chi <= 2 && Me.HealthPercent <= 80, "Expel Harm"),
+                           Spell.CastSpell("Jab", ret => Chi <= 2 && Me.HealthPercent > 80 || Chi <= 2 && Spell.SpellOnCooldown("Expel Harm") && Me.HealthPercent <= 80, "Jab"),
                             //Spell.CastSpell("Blackout Kick", ret => (Me.CurrentEnergy > 30 && RisingSunKickCoolDown) || (Chi > 4 && Buff.PlayerHasActiveBuff("Ascension")) || (Chi > 5 && Buff.PlayerHasActiveBuff("Ascension")), "Blackout Kick"),
 
                            // Interupt
