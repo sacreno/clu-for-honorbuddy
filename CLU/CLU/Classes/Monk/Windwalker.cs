a07532ea4fd0657df69ce339d6fd0465a2243100
tuanhawow@gmail.com <tuanhawow@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-26 10:17:56 +0000

    Add Interrupt Hand Spear Strike to Windwalker.cs

--- CLU/CLU/Classes/Monk/Windwalker.cs
+++ CLU/CLU/Classes/Monk/Windwalker.cs
@@ -65,9 +65,7 @@ namespace CLU.Classes.Monk
 
         private static uint Chi
         {
-            get
-            {
-                return Me.CurrentChi; // StyxWoW.Me.GetCurrentPower(WoWPowerType.LightForce);
+            get { return Me.CurrentChi; // StyxWoW.Me.GetCurrentPower(WoWPowerType.LightForce);
             }
         }
 
@@ -79,7 +77,8 @@ namespace CLU.Classes.Monk
         // TODO: CHECK JAB IS NOT AFFECTED BY THE WEAPON YOU ARE CARRYING AND WE ONLY NEED TO USE JAB AND THE SPELLID AND ICON WILL CHANGE.
 
 
-        private static readonly List<string> JabSpellList = new List<string> { "Jab", "Club", "Slice", "Sever", "Pike", "Clobber" };
+        private static readonly List<string> JabSpellList = new List<string>
+                                                                {"Jab", "Club", "Slice", "Sever", "Pike", "Clobber"};
 
         private static bool RisingSunKickCoolDown
         {
@@ -104,6 +103,7 @@ namespace CLU.Classes.Monk
                             Item.UseEngineerGloves())),
                     //Single Target
                     Spell.CastSpell("Touch of Death", ret => Me.HasAura("Death Note"), "Touch of Death"),
+                    Spell.CastInterupt("Spear Hand Strike", ret => true, "Spear Hand Strike"),
                     Spell.CastSelfSpell("Tigereye Brew",
                                         ret =>
                                         !Buff.PlayerHasBuff("Tigereye Brew Use") &&
@@ -113,7 +113,10 @@ namespace CLU.Classes.Monk
                     Spell.CastSpell("Energizing Brew", ret => Me.CurrentEnergy <= 30, "Energizing Brew"),
                     Spell.CastSpell("Expel Harm", ret => Me.HealthPercent < 80 && Me.CurrentEnergy >= 40 && Chi <= 2,
                                     "Expel Harm"),
-                    Spell.CastSpell("Disable", ret => Me.CurrentEnergy >= 15 && (Me.CurrentTarget.IsPlayer || Me.CurrentTarget.Fleeing) && Me.CurrentTarget.MovementInfo.RunSpeed > 3.5,
+                    Spell.CastSpell("Disable",
+                                    ret =>
+                                    Me.CurrentEnergy >= 15 && (Me.CurrentTarget.IsPlayer || Me.CurrentTarget.Fleeing) &&
+                                    Me.CurrentTarget.MovementInfo.RunSpeed > 3.5,
                                     "Disable"),
                     Spell.CastSpell("Rising Sun Kick",
                                     ret =>
@@ -185,9 +188,9 @@ namespace CLU.Classes.Monk
                     ret => Me.HealthPercent < 100 && CLUSettings.Instance.EnableSelfHealing,
                     new PrioritySelector(
                         Buff.CastBuff("Fortifying Brew", ret => Me.HealthPercent < 50, "Fortifying Brew"),
-                    // Turns your skin to stone, increasing your health by 20%, and reducing damage taken by 20%. Lasts 20 sec.
+                        // Turns your skin to stone, increasing your health by 20%, and reducing damage taken by 20%. Lasts 20 sec.
                         Buff.CastBuff("Guard", ret => Me.HealthPercent < 50, "Guard"),
-                    // absorbs damage for 30secs and increases any healing by 30%
+                        // absorbs damage for 30secs and increases any healing by 30%
                         Item.UseBagItem("Healthstone", ret => Me.HealthPercent < 40, "Healthstone")));
             }
         }
