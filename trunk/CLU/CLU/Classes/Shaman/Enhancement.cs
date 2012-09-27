ca4cab072ccc99f8e447f09229e000f4df8fe59c
alex.ownage@gmail.com <alex.ownage@gmail.com@504384ae-b32f-b524-21f4-081d471b9747>
2012-09-27 11:26:51 +0000

    Another Magma totem fix. No matter if there's a searing totem, magma should always be there if more than 3 mobs.

--- trunk/CLU/CLU/Classes/Shaman/Enhancement.cs
+++ trunk/CLU/CLU/Classes/Shaman/Enhancement.cs
@@ -124,7 +124,8 @@ namespace CLU.Classes.Shaman
                     // Totem management
                            Totems.CreateTotemsBehavior(),
                     // AoE
-                                   Spell.CastTotem("Magma Totem", ret => Me.CurrentTarget != null && Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 8) >= 3 && !Totems.Exist(WoWTotemType.Fire), "Magma Totem"),
+                                   Spell.CastTotem("Magma Totem", ret => Me.CurrentTarget != null && Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 8) >= 3 && !Totems.Exist(WoWTotem.Magma), "Magma Totem"),
+                                   Spell.CastTotem("Magma Totem", ret => Me.CurrentTarget != null && Unit.CountEnnemiesInRange(Me.CurrentTarget.Location, 8) >= 3 && Totems.Exist(WoWTotem.Searing) && !Totems.Exist(WoWTotem.Magma), "Magma Totem"),
                                    Spell.CastAreaSpell("Chain Lightning", 5, false, 3, 0.0, 0.0, a => Buff.PlayerCountBuff("Maelstrom Weapon") > 4, "Chain Lightning"),
                                    Spell.CastSpell("Flame Shock", ret => !Buff.TargetHasDebuff("Flame Shock") || Buff.TargetDebuffTimeLeft("Flame Shock").Seconds <= 3, "Flame Shock"),
                                    Spell.CastSpell("Flame Shock", ret => Buff.PlayerHasActiveBuff("Unleash Flame"), "Flame Shock"),
